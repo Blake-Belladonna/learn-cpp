27ae3498ebcab426518642dd8f88238427c06c8f
KeWen <13782823158@163.com>
2021-01-24 10:59:50 +0800

    learn about functions.

---
+++ AABB形式完全平方数的求法/AABB形式完全平方数的求法.cpp
@@ -0,0 +1,62 @@
+﻿// AABB形式完全平方数的求法.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void function_01();// 正向求解
+void function_02();// 逆向求解
+
+int main()
+{
+	function_01();
+	function_02();
+
+	std::cout << "Hello World!\n";
+}
+
+void function_01()
+{
+	unsigned int n = 0;
+	for (unsigned int i = 1; i < 10; i++)
+	{
+		for (unsigned int j = 0; j < 10; j++)
+		{
+			n = i * 1100 + j * 11;
+			double sqrtRet = sqrt(n);
+			sqrtRet -= int(sqrtRet);
+			if (sqrtRet < 0.00000001) //判断平方根结果是否为整数（不推荐）
+			{
+				cout << "找到AABB类型完全平方数：" << n << endl;
+			}
+		}
+	}
+}
+
+void function_02()
+{
+	unsigned int n = 0, high = 0, low = 0;
+	for (unsigned int i = 31; i < 100; i++)
+	{
+		n = i * i;
+		high = n / 100;
+		low = n % 100;
+		if ((high / 10 == high % 10) && (low / 10 == low % 10))
+		{
+			cout << "找到AABB类型完全平方数：" << n << endl;
+		}
+	}
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ 函数指针/函数指针.cpp
@@ -0,0 +1,58 @@
+﻿// 函数指针.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+using namespace std;
+
+int max(int, int);
+int min(int, int);
+int add(int, int);
+int process(int, int, int(*)(int, int));
+
+int main()
+{
+	int(*functionPointer)(int, int);
+	functionPointer = add;
+	//(*function) 和 function 的地址值一致，所以以下两种调用方式都可以
+	cout << "function() = " << functionPointer(2, 8) << endl;
+	cout << "function() = " << (*functionPointer)(2, 8) << endl;
+
+	int a = 3, b = 5;
+	cout << "process(a, b, max) = " << process(a, b, max) << endl;
+	cout << "process(a, b, min) = " << process(a, b, min) << endl;
+	cout << "process(a, b, add) = " << process(a, b, add) << endl;
+
+	std::cout << "Hello World!\n";
+}
+
+int max(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+int min(int a, int b)
+{
+	return a < b ? a : b;
+}
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int process(int a, int b, int(*p)(int, int))
+{
+	return p(a, b);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
